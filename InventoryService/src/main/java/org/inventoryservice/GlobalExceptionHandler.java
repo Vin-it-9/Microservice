7d511328db9d06f8de1d03a61737343c733de28c
Vin-it-9 <vineetshinde2559@gmail.com>
2025-02-15 16:53:06 +0530

    commit

--- InventoryService/src/main/java/org/inventoryservice/GlobalExceptionHandler.java
+++ InventoryService/src/main/java/org/inventoryservice/GlobalExceptionHandler.java
@@ -12,8 +12,8 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handleUserNotFound(UserNotFoundException ex) {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFound(ProductNotFoundException ex) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("message", ex.getMessage());
         body.put("timestamp", LocalDateTime.now());
--- OrderService/src/main/java/org/orderservice/OrderServiceApplication.java
+++ OrderService/src/main/java/org/orderservice/OrderServiceApplication.java
@@ -2,10 +2,11 @@ package org.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
